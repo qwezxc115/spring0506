c94bcc6cd47dd0f67b16bd15d8a45b002f58de73
qwezxc115 <qwezxc115@naver.com>
2022-05-11 16:38:25 +0900

    0511

---
+++ src/main/java/org/zerock/controller/ex08/Ex12Controller.java
@@ -0,0 +1,39 @@
+package org.zerock.controller.ex08;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.controller.ex03.Employee;
+import org.zerock.service.ex01.Ex01Service;
+import org.zerock.service.ex01.Ex02Service;
+
+@Controller
+@RequestMapping("ex12")
+public class Ex12Controller {
+	
+	@Autowired
+	private Ex01Service service;
+	
+//	@Setter(Method_ = @Autowired) : 책
+	@Autowired
+	private Ex02Service service2;
+
+	@RequestMapping("sub01")
+	public void method01() {
+		// 1. request 파라미터 수집/가공
+		
+		// 2. bussiness 로직 실행
+		service.getCustomer(1);
+		
+		// 3. model에 attribute 추가
+		
+		// 4. forward / redirect
+	}
+	
+	@RequestMapping("sub02")
+	public void method02(Employee emp) {
+		service2.insertEmployee(emp);
+		
+		System.out.println("입력 성공...");
+	}
+}
---
+++ src/main/java/org/zerock/domain/ex01/EmployeeDto.java
@@ -0,0 +1,12 @@
+package org.zerock.domain.ex01;
+
+import lombok.Data;
+
+@Data
+public class EmployeeDto {
+	private String firstName;
+	private String lastName;
+	private String birthDate;
+	private String photo;
+	private String notes;
+}
